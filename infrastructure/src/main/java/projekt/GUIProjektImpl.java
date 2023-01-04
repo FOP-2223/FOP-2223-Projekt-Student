5a7382c297fc91443970ee58a6614423937618ce
LukasKlenner <Klenner-lukas@t-online.de>
2023-01-04 01:15:37 +0100

    refactor IOHelper methods

--- infrastructure/src/main/java/projekt/GUIProjektImpl.java
+++ infrastructure/src/main/java/projekt/GUIProjektImpl.java
@@ -2,11 +2,13 @@ package projekt;
 
 import javafx.application.Application;
 import projekt.gui.MyApplication;
+import projekt.io.IOHelper;
 
 @SuppressWarnings("unused")
 public class GUIProjektImpl implements Projekt {
     @Override
     public void start() {
+        IOHelper.initProblemPresets();
         Application.launch(MyApplication.class);
     }
 }
---
+++ infrastructure/src/main/java/projekt/io/IOHelper.java
@@ -0,0 +1,101 @@
+package projekt.io;
+
+import projekt.delivery.archetype.ProblemArchetype;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.*;
+
+/**
+ * A static helper class for handling io operations containing {@link ProblemArchetype}s.
+ */
+public class IOHelper {
+
+    public static final File PROBLEMS_DIR = Path.of(System.getProperty("user.dir"), "projekt", "gui", "problems").toFile();
+
+    /**
+     * Copies the {@link ProblemArchetype} presets from the resource directory into the build directory.
+     */
+    public static void initProblemPresets() {
+        // problem 1
+        try (InputStream problem1 = IOHelper.class.getResourceAsStream("presets/problem1.txt");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(problem1)))) {
+            writeProblem(ProblemArchetypeIO.readProblemArchetype(reader));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // problem 2
+        try (InputStream problem2 = IOHelper.class.getResourceAsStream("presets/problem2.txt");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(problem2)))) {
+            writeProblem(ProblemArchetypeIO.readProblemArchetype(reader));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns all {@link ProblemArchetype}s stored in the resource and the build directory.
+     *
+     * @return All {@link ProblemArchetype}s stored in the resource and the build directory.
+     * @see #getAllProblemsInBuildDir()
+     */
+    public static List<ProblemArchetype> readProblems() {
+        return readProblemsInFiles(getAllProblemsInBuildDir());
+    }
+
+    /**
+     * Returns a {@link Set} of {@link File}s containing all {@link ProblemArchetype}s stored in the build dir (build/run/projekt/gui/problems).
+     *
+     * @return A {@link Set} of {@link File}s containing all {@link ProblemArchetype}s stored in the build dir.
+     */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static Set<File> getAllProblemsInBuildDir() {
+        File dir = new File(Path.of(System.getProperty("user.dir"), "projekt", "gui", "problems").toUri());
+
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        return new HashSet<>(Arrays.asList(Objects.requireNonNull(dir.listFiles())));
+    }
+
+    private static List<ProblemArchetype> readProblemsInFiles(Set<File> files) {
+        List<ProblemArchetype> problems = new ArrayList<>();
+
+        for (File file : files) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                problems.add(ProblemArchetypeIO.readProblemArchetype(reader));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return problems;
+    }
+
+    /**
+     * Writes the given {@link ProblemArchetype} into a text file in the build directory (build/run/projekt/gui/problems).<p>
+     * <p>
+     * The name of the file will be the name of the {@link ProblemArchetype}. The content of the file will be
+     * produced by the {@link ProblemArchetypeIO#writeProblemArchetype(BufferedWriter, ProblemArchetype) method.
+     * <p>
+     *
+     * @param problem The {@link ProblemArchetype} to write into the file.
+     */
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void writeProblem(ProblemArchetype problem) {
+        if (!PROBLEMS_DIR.exists()) {
+            PROBLEMS_DIR.mkdirs();
+        }
+
+        File file = Path.of(PROBLEMS_DIR.getPath(), problem.name() + ".txt").toFile();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            file.createNewFile();
+            ProblemArchetypeIO.writeProblemArchetype(writer, problem);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
